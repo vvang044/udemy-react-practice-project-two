b0b79b94d6969ca6c21d1edc83cfa15d714deff6
Vik <vvang044@gmail.com>
2021-09-27 18:17:41 -0400

    92. Button, Button UI

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import Button from "../UI/Button";
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
@@ -19,7 +20,7 @@ const AddUser = (props) => {
                 <label htmlFor="age"> Age( Years )</label>
                 <input id="age" type="number"></input>
 
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     )
