d63a514aa4b87b1267f2309283502682e207ef86
Vik <vvang044@gmail.com>
2021-09-29 21:15:57 -0400

    102. React Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, Fragment} from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
@@ -18,10 +18,10 @@ import UsersList from "./components/Users/UsersList";
     };
   
     return (
-      <div>
+      <Fragment>
         <AddUser onAddUser={addUserHandler} />
         <UsersList users={usersList} />
-      </div>
+      </Fragment>
     );
   }
 
--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -1,5 +1,8 @@
+
 const Wrapper = (props) => {
   return  props.children
 }
 
 export default Wrapper
+
+
